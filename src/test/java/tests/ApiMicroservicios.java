3e7284b7fa2e7b73e3f08a3eb467b789e3a3b3df
joelLopez <joel.a.lopezf@outlook.com>
2025-06-15 01:09:08 -0600

    ci updated 5

--- src/test/java/tests/ApiMicroservicios.java
+++ src/test/java/tests/ApiMicroservicios.java
@@ -11,25 +11,32 @@ public class ApiMicroservicios {
     @Test
     public void testGetUsers(){
         UserService userApi = new UserService();
+        System.out.println("\n\nMetodo GET\n\n");
         Response getResponse = userApi.GetUser(1);
 
         String email = getResponse.jsonPath().getString("data.email");
         System.out.println("email valido: " + email);
-        Assert.assertTrue(email.contains("@"), "El email no contiene el caracter '@'");
+        Assert.assertTrue(email.contains("@"), "El email no contiene el caracter '@'\n\n");
     }
     @Test
     public void testPostUsers(){
         UserService userApi = new UserService();
+        System.out.println("\n\nMetodo POST\n\n");
         Response getResponse = userApi.PostUsers();
+        System.out.println("\n\n");
     }
     @Test
     public void testPutUser(){
         UserService userApi = new UserService();
+        System.out.println("\n\nMetodo PUT\n\n");
         Response getResponse = userApi.PutUsers(2);
+        System.out.println("\n\n");
     }
     @Test
     public void testDeleteUser(){
         UserService userApi = new UserService();
+        System.out.println("\n\nMetodo DELETE\n\n");
         Response getResponse = userApi.DeleteUsers(2);
+        System.out.println("\n\n");
     }
 }
